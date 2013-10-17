ce386e8fc2fc11f1d450885505395259c380c0b7
Angela <angelahdezd@gmail.com>
2013-10-17 15:02:40 +0100

    fin

--- rps.rb
+++ rps.rb
@@ -2,12 +2,10 @@ require './lib/rps.rb'
 
 
 builder = Rack:: Builder.new do
-use Rack::Static, :urls => ['/public']
-use Rack::ShowExceptions
-use Rack::Lint
-
-
-run RockPaperScissors::App.new
+  use Rack::Static, :urls => ['/public']
+  use Rack::ShowExceptions
+  use Rack::Lint
+  run RockPaperScissors::App.new
 end
 
 Rack::Handler::Thin.run builder
